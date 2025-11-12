4e14d6b822866c0603ca72e02c7aeed81dfb91a1
Willson Tang <180681710+WillsonT2@users.noreply.github.com.>
2025-11-12 14:48:46 -0500

    second

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -21,6 +22,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -28,7 +33,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -37,6 +44,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todolistapp.ui.theme.ToDoListAppTheme
+import kotlinx.coroutines.launch
+import kotlin.text.get
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,69 +60,118 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ToDoListApp() {
     val toDoManager = ToDoManager()
-    Surface(
-        color = Color.LightGray,
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Gray)
-                    .padding(16.dp)
-            ){
-                var enteredText by remember { mutableStateOf("") }
-                TextField(
-                    value = enteredText,
-                    onValueChange = {
-                        enteredText = it
-                    },
-                    singleLine = true,
-                    label = { Text(text = "Add new task") },
-                    placeholder = { Text(text = "Type your task") },
-                    textStyle = TextStyle(
-                        color = Color(230, 160, 70),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp
-                    ),
-                    modifier = Modifier.weight(1.0F)
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Button(
-                    onClick = {
-                        toDoManager.addTask(enteredText)
-                        enteredText = ""
-                    }
-                ) {
-                    Text(
-                        text = "Add"
-                    )
-                }
-            }
-            LazyColumn(
-                contentPadding = PaddingValues(all = 16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(toDoManager.getTasks()){task ->
-                    Card(
-                        colors = CardDefaults.cardColors(
-                            containerColor = Color(230, 160, 70),
-                            contentColor = Color.White
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { contentPadding ->
+        Surface(
+            color = Color.LightGray,
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Gray)
+                        .padding(16.dp)
+                ){
+                    var enteredText by remember { mutableStateOf("") }
+                    TextField(
+                        value = enteredText,
+                        onValueChange = {
+                            enteredText = it
+                        },
+                        singleLine = true,
+                        label = { Text(text = "Add new task") },
+                        placeholder = { Text(text = "Type your task") },
+                        textStyle = TextStyle(
+                            color = Color(230, 160, 70),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
                         ),
-                        elevation = CardDefaults.cardElevation(
-                            defaultElevation = 8.dp
-                        )
-                    ){
+                        modifier = Modifier.weight(1.0F)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(
+                        onClick = {
+                            toDoManager.addTask(enteredText, 0)
+                            enteredText = ""
+                        }
+                    ) {
                         Text(
-                            text = task,
-                            fontSize = 28.sp,
-                            modifier = Modifier.padding(8.dp)
+                            text = "Add"
                         )
+                    }
+                }
+                if (toDoManager.getTasks().isEmpty()){
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(
+                            text = "Add your first task!",
+                            color = Color(230, 160, 70),
+                            fontSize = 36.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }else {
+                    LazyColumn(
+                        contentPadding = PaddingValues(all = 16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        items(toDoManager.getTasks().size){index ->
+                            TextRow(toDoManager.getTasks()[index], modifier = Modifier.clickable(
+                                onClick = {
+                                    val removed = toDoManager.getTasks()[index]
+                                    toDoManager.removeTask(index)
+                                    scope.launch {
+                                        val result: SnackbarResult = snackbarHostState.showSnackbar(
+                                            message = "Task Completed",
+                                            actionLabel = "Undo",
+                                            duration = SnackbarDuration.Short
+                                        )
+                                        when (result){
+                                            SnackbarResult.ActionPerformed ->{
+                                                toDoManager.addTask(removed, index)
+                                            }
+                                            SnackbarResult.Dismissed ->{
+
+                                            }
+                                        }
+                                    }
+                                }
+                            ))
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TextRow(taskText: String, modifier: Modifier){
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color(230, 160, 70),
+            contentColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = modifier
+    ) {
+        Text(
+            text = taskText,
+            fontSize = 28.sp,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
--- app/src/main/java/com/example/todolistapp/ToDoManager.kt
+++ app/src/main/java/com/example/todolistapp/ToDoManager.kt
@@ -3,33 +3,20 @@ package com.example.todolistapp
 import androidx.compose.runtime.mutableStateListOf
 
 class ToDoManager {
-    private val toDoTasks: MutableList<String> = mutableStateListOf("" +
-            "Paint cabinet",
-            "Buy groceries",
-            "Clean my room",
-            "Do the laundry",
-            "Call mom",
-            "Stop by post office",
-            "Pay water bill",
-            "Wash dirty dishes",
-            "Vacuum carpet",
-            "Fix leaking faucet",
-            "Attend meeting on Friday",
-            "Buy tickets for game",
-            "Pick up bread at bakery",
-            "Walk dog",
-            "Do my homework",
-            "Brush teeth"
-    )
+    private val toDoTasks: MutableList<String> = mutableStateListOf()
+
 
     fun getTasks(): List<String>{
         return toDoTasks.toList()
     }
 
-    fun addTask(str: String){
-        toDoTasks.add(0, str)
+    fun addTask(str: String, index: Int){
+        toDoTasks.add(index, str)
     }
 
+    fun removeTask(index: Int){
+        toDoTasks.removeAt(index)
+    }
 }
 
 
